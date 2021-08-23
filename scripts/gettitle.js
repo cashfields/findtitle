fe4f463a378e0cdd4e7945784cab2f6c656a5ae3
Scott CA$H Fields <cashfieldsutc@gmail.com>
2021-08-22 21:06:10 -0500

    Update gettitle.js

--- scripts/gettitle.js
+++ scripts/gettitle.js
@@ -1,60 +1,31 @@
-function myFunction() 
-{
-  alert("Here Top");
-  
-  
-  var x = document.getElementsByTagName("title")[0];
 
-alert(x.innerHTML)
+  <script>
+	//https://stackoverflow.com/questions/40858456/how-to-display-a-javascript-var-in-html-body	//
+	//https://stackoverflow.com/questions/40858456/how-to-display-a-javascript-var-in-html-body//
+	function myFunction() 
+	{
+	var theHead = document.head;
+	console.log(theHead.id); // "my-document-head";
+	console.log( theHead === document.querySelector("head") ); // true
+	//alert(theHead.id);
+	//alert("skip")
+	var wantedname = (theHead.id);
+	alert(wantedname);
+	document.getElementById("mywantedname").innerHTML = wantedname;
+	var x = document.title;
+	alert(x);
+	document.getElementById("myx").innerHTML = x
+	console.log(x); 
+	}
+	 </script>
 
-// or
 
-alert(x.textContent)
 
-// or
-xx = document.getElementsByTagName("title")
-alert(document.getElementsByTagName("title").lastChild)
-  
-  alert(xx.lastChild)
-  alert("Here mid")
-  
-  
-  
-  
-  var x = document.getElementsByTagName("title");
 
-alert(x.innerHTML)
 
-// or
 
-alert(x.textContent)
 
-// or
 
-zz = document.getElementsByTagName("title")
-  alert(document.getElementsByTagName("title").innerHTML)
-  //alert(zz.innerText)
- alert("Here mid2")
-  
-  
-  
-  
-  console.log(document.title)
-  alert(document.title)
- 
-var theHead = document.head;
-console.log(theHead.id); // "my-document-head";
-console.log( theHead === document.querySelector("head") ); // true
-//alert(theHead.id);
-//alert("skip")
-var wantedname = (theHead.id);
-alert(wantedname);
-document.getElementById("mywantedname").innerHTML = wantedname;
 
-var x = document.title;
-alert(x);
-document.getElementById("myx").innerHTML = x
-console.log(x); 
-  
-  alert("Here Bot");
-}
+
+
